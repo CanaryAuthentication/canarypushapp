058fbe5dee9c2974c857fe2e21edf34657c45973
Haiwei Su <haiwei93@gmail.com>
2015-11-02 15:11:08 -0500

    this is bluetooth detection with push note app

---
+++ appcode/BluetoothCanaryApp3/BluetoothCanaryApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BluetoothCanaryApp
+//
+//  Created by Jeff Ponnor and Haiwei Su on 10/23/15.
+//  Copyright © 2015 Jeff Ponnor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
